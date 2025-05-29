628144689de3ac55f9e21aa925690f50895e1eef
Phinhas Asmelash <phinhasasmelash@gmail.com>
2025-05-28 23:58:29 -0700

    🖌️ added to game board layout

--- src/gameBoard.lua
+++ src/gameBoard.lua
@@ -34,6 +34,10 @@ function GameBoard:draw()
   
   self:drawBackground()
   
+  -- TODO: if mouse hover over card set text to card info
+  -- TODO: pass card as a parameter
+  self:setText()
+  
   -- draw deck pile
   for i=1, #self.playerDeck do
     self.playerDeck[i]:draw()
@@ -44,10 +48,6 @@ function GameBoard:draw()
     self.playArea[i]:draw()
   end
   
---  for _, cards in ipairs(self.hands) do
---    cards:draw()
---  end
-  
   for i = 1, #self.hands do
     self.hands[i]:draw()
   end
@@ -56,12 +56,6 @@ function GameBoard:draw()
   for i=1, #self.pickedUpCards do
     self.pickedUpCards[i]:draw()
   end
-  
-  
-  -- TODO: if mouse hover over card set text to card info
-  -- TODO: pass card as a parameter
-  self:setText()
-  
   
 end
 
@@ -101,12 +95,50 @@ function GameBoard:setText()
   
   love.graphics.setFont(font)
   
+  love.graphics.printf("Player", 915, 10, 275, 'left')
+  love.graphics.printf("Enemy", 905, 10, 275, 'right')
+  local playerPoints = 15
+  local AIPoints = 10
+  love.graphics.printf(playerPoints, 915, 50, 275, 'left')
+  love.graphics.printf(AIPoints, 905, 50, 275, 'right')
+  
+  love.graphics.printf("Mana", 915, 110, 275, 'left')
+  local playerMana = 4
+  love.graphics.printf(playerMana, 915, 150, 275, 'left')
+  
+  love.graphics.printf("Turn", 905, 110, 275, 'right')
+  local turnNum = 4
+  love.graphics.printf(turnNum, 905, 150, 275, 'right')
+  
+  
+  love.graphics.printf("Cost", 915, 208, 275, 'left')
+  local cardCost = 2
+  love.graphics.printf(cardCost, 915, 248, 275, 'left')
+  
+  love.graphics.printf("Power", 905, 208, 275, 'right')
+  local cardPower = 3
+  love.graphics.printf(cardPower, 905, 248, 275, 'right')
+  
+  
   local description = "Hover over a card for details."
   love.graphics.printf("Card Description", 915, 315, 275, 'center')
   love.graphics.printf(description, 915, 350, 275, 'left')
+  
+  love.graphics.setColor(0.7, 0.7, 1)
+  love.graphics.rectangle("fill", 950, 615, 200, 70)
+  love.graphics.setColor(1, 1, 1)
+  love.graphics.printf("Submit Play", 900, 630, 300, 'center')
+  
+  
+  
+  
+  
+  
 end
 
 function GameBoard:drawBackground()
+  
+  love.graphics.setColor(1, 1, 1)
   self:renderLayoutDividers()
   
   self:renderAILocations()
